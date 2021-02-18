3d38f2bbd9a92b0ab60a948a9b20444d5cd6f86c
sarah-svg <kjskillen@gmail.com>
2021-02-17 18:12:42 -0800

    tested all of the containor

--- src/containor/Dog.test.jsx
+++ src/containor/Dog.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 global.fetch = require('node-fetch');
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
@@ -10,15 +11,20 @@ describe.only('Dog containor', () => {
     const postMethod = screen.getByPlaceholderText('Enter post');
     const name = screen.getByPlaceholderText('Enter name');
     const inputText = screen.getByPlaceholderText('Enter type');
-    // eslint-disable-next-line max-len
     const inputCharacteristic = screen.getByPlaceholderText('Enter characteristic');
     const getMethod = screen.getByText('Get all Dogs');
+    const getMethodID = screen.getByText('Find Dog by Id');
+    const updateDog = screen.getByText('Update dog by Id');
+    const deleteDog = screen.getByText('Delete Dog by Id');
 
     user.type(inputCharacteristic);
     user.type(postMethod);
     user.type(name);
     user.type(inputText);
     user.type(getMethod);
+    user.type(getMethodID);
+    user.type(updateDog);
+    user.type(deleteDog);
 
     return waitFor(() => {
       expect(screen.getByText('Dog')).not.toBeEmptyDOMElement();
